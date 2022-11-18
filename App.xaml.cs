2acf9099c2cb6d97e579569e503b66c171a8dd1e
Luigi Carvalho <carvalholuigi25@gmail.com>
2022-11-18 10:18:08 +0000

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace LCPMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LCPMaui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
